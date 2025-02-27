72a79442cdd26ff21fce9b1ac970669b8e04454c
MykolaKud <kudinov.myk@gmail.com>
2025-02-27 11:58:10 +0200

    Update and rename 11. Distribution of payment installmenst.sql to Analysis/11. Distribution_of_payment_installmenst.sql

--- 11. Distribution of payment installmenst.sql
+++
@@ -1,7 +0,0 @@
-# What is the distribution of installment payments? 
-select 
-  payment_installments,
-  count(*) as num_of_payments
-from olist_order_payments_dataset
-group by payment_installments
-order by num_of_payments desc;
---
+++ Analysis/11. Distribution_of_payment_installmenst.sql
@@ -0,0 +1,8 @@
+# What is the distribution of installment payments? 
+  
+SELECT 
+  payment_installments,
+  COUNT(*) AS num_of_payments
+FROM olist_order_payments_dataset
+GROUP BY payment_installments
+ORDER BY num_of_payments DESC;
