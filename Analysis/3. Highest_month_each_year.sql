a59ae8871ded64e3b8ba309e12d4bff1516b0094
MykolaKud <kudinov.myk@gmail.com>
2025-02-27 11:34:09 +0200

    Update and rename 3. Highest_month_each_year.sql to Analysis/3. Highest_month_each_year.sql

---
+++ Analysis/3. Highest_month_each_year.sql
@@ -0,0 +1,19 @@
+# Let's check which month had the highest number of orders each year except canceled orders?
+  
+WITH orders_grouped_by_month AS (
+  SELECT 
+    MONTH(order_purchase_timestamp) AS order_month,
+    YEAR(order_purchase_timestamp) AS order_year,
+    COUNT(*) AS num_of_orders
+  FROM olist_orders_dataset
+  WHERE order_status <> 'canceled'
+  GROUP BY order_month, order_year
+)
+SELECT * FROM orders_grouped_by_month
+WHERE num_of_orders = (
+  SELECT
+    MAX(num_of_orders)
+  FROM orders_grouped_by_month AS og
+  WHERE og.order_year = orders_grouped_by_month.order_year
+)
+ORDER BY order_year, order_month;
