a7909eb368400417bd9abbea529d1a73aef1ab8d
MykolaKud <kudinov.myk@gmail.com>
2025-02-28 16:19:42 +0200

    Update 2. Sum_per_category.sql

--- Analysis/2. Sum_per_category.sql
+++ Analysis/2. Sum_per_category.sql
@@ -1,8 +1,7 @@
-#Let's calculate sum of orders per product category named in English
-    
 SELECT 
     pnt.product_category_name_english AS product_category,
-    COUNT(ood.order_id) AS sum_of_orders
+    COUNT(ood.order_id) AS sum_of_orders,
+    ROUND((COUNT(*)*100.0)/(SELECT COUNT(*) FROM olist_orders_dataset), 2) AS percent_from_total    
 FROM olist_orders_dataset ood
 JOIN olist_order_items_dataset oid ON oid.order_id  = ood.order_id
 JOIN olist_products_dataset opd ON opd.product_id = oid.product_id 
