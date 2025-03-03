4b329afc34291879b4940aa5ae85718d73fbd51c
MykolaKud <kudinov.myk@gmail.com>
2025-03-03 15:25:49 +0200

    Update 8. Average_shipping_time_per_state.sql

--- Analysis/8. Average_shipping_time_per_state.sql
+++ Analysis/8. Average_shipping_time_per_state.sql
@@ -1,14 +1,12 @@
-#Let's calculate the average shipping time per state
-
 SELECT
   ocd.customer_state,
-  ROUND(AVG(DATEDIFF(ood.order_estimated_delivery_date, ood.order_delivered_carrier_date)), 0) AS shipping_time
+  ROUND(AVG(DATEDIFF(ood.order_estimated_delivery_date, ood.order_delivered_carrier_date)), 0) AS shipping_time_days
 FROM olist_orders_dataset ood
 JOIN olist_customers_dataset ocd ON ood.customer_id = ocd.customer_id 
 WHERE ood.order_status = 'delivered' 
   AND order_estimated_delivery_date <> '' 
   AND order_delivered_carrier_date <> ''
 GROUP BY ocd.customer_state
-ORDER BY shipping_time DESC;
+ORDER BY shipping_time;
 
 
