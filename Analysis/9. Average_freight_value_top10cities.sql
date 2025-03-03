54288767636681c106ec3139aa34c59f87b8897b
MykolaKud <kudinov.myk@gmail.com>
2025-03-03 15:44:04 +0200

    Update 9. Average_freight_value_top10cities.sql

--- Analysis/9. Average_freight_value_top10cities.sql
+++ Analysis/9. Average_freight_value_top10cities.sql
@@ -1,12 +1,10 @@
-# Which 10 cities have the highest shipping costs on average?
-
 SELECT 
-  ocd.customer_city,
+  ocd.customer_state,
   ROUND(AVG(ooid.freight_value), 2) AS average_freight_value
 FROM olist_order_items_dataset ooid
 JOIN olist_orders_dataset ood ON ooid.order_id = ood.order_id 
 JOIN olist_customers_dataset ocd ON ood.customer_id = ocd.customer_id
 WHERE ooid.freight_value > 0
-GROUP BY ocd.customer_city
+GROUP BY ocd.customer_state
 ORDER BY average_freight_value DESC
-LIMIT 10;
+LIMIT 7;
