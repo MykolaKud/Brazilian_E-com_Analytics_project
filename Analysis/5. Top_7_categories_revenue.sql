cf5539bb4d243dede52facd03ffebeaf298ee2f8
MykolaKud <kudinov.myk@gmail.com>
2025-03-03 10:56:22 +0200

    Update 5. Top_7_categories_revenue.sql

--- Analysis/5. Top_7_categories_revenue.sql
+++ Analysis/5. Top_7_categories_revenue.sql
@@ -1,8 +1,7 @@
-# Which top 7 product categories generated the highest revenue?
-
 SELECT
   opd.product_category_name,
-  ROUND(SUM(oopd.payment_value), 2) AS revenue
+  ROUND(SUM(oopd.payment_value), 2) AS revenue,
+  ROUND((SUM(oopd.payment_value)*100.0)/(SELECT SUM(payment_value) FROM olist_order_payments_dataset), 2) AS percent_from_total
 FROM olist_order_payments_dataset oopd
 JOIN olist_orders_dataset ood ON oopd.order_id = ood.order_id
 JOIN olist_order_items_dataset ooid ON oopd.order_id = ooid.order_id
