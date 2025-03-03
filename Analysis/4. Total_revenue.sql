1b547a1d3802195faec89e6afd7f3e2214a2b560
MykolaKud <kudinov.myk@gmail.com>
2025-03-03 10:52:45 +0200

    Update 4. Total_revenue.sql

--- Analysis/4. Total_revenue.sql
+++ Analysis/4. Total_revenue.sql
@@ -1,8 +1,8 @@
-# What is the total revenue generated per year?
-
 SELECT
   YEAR(order_purchase_timestamp) AS reporting_year,
-  ROUND(SUM(oopd.payment_value), 2) AS annual_revenue
+  ROUND(SUM(oopd.payment_value), 2) AS annual_revenue,
+  COUNT(ood.order_id) AS total_orders_per_year,
+  ROUND(AVG(oopd.payment_value), 2) AS average_order_value
 FROM olist_orders_dataset ood
 JOIN olist_order_payments_dataset oopd ON ood.order_id = oopd.order_id 
 WHERE ood.order_status <> 'canceled'
