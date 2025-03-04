90f16a949662b5226ddc5d796e0aa59017ba4c01
MykolaKud <kudinov.myk@gmail.com>
2025-03-04 10:00:57 +0200

    Update Checking order_id for duplicates reason.sql

--- Preparing_dataset_for analysis/Checking order_id for duplicates reason.sql
+++ Preparing_dataset_for analysis/Checking order_id for duplicates reason.sql
@@ -1,8 +1,10 @@
+#Selecting dulicate order_id's in payments dataset
 SELECT order_id
 FROM olist_order_payments_dataset
 GROUP BY order_id
 HAVING COUNT(*) > 1;
 
+#Checking 4 first id's  
 SELECT * FROM olist_order_payments_dataset
 WHERE order_id  = '5cfd514482e22bc992e7693f0e3e8df7' 
 OR order_id  = 'b2bb080b6bc860118a246fd9b6fad6da' 
