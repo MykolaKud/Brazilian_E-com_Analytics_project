7ea7141f3fe5389e767ec2a31feb90a1d5a27f7f
MykolaKud <kudinov.myk@gmail.com>
2025-03-04 09:52:19 +0200

    Update Checking review_id for duplicates reason.sql

--- Preparing_dataset_for analysis/Checking review_id for duplicates reason.sql
+++ Preparing_dataset_for analysis/Checking review_id for duplicates reason.sql
@@ -16,8 +16,7 @@ SELECT * FROM olist_order_reviews_dataset WHERE review_id = '00130cbe1f9d422698c
 SELECT review_id
 FROM olist_order_reviews_dataset
 GROUP BY review_id
-HAVING COUNT(DISTINCT order_id) > 1
-   OR COUNT(DISTINCT review_score) > 1
+HAVING COUNT(DISTINCT review_score) > 1
    OR COUNT(DISTINCT review_comment_title) > 1
    OR COUNT(DISTINCT review_comment_message) > 1
    OR COUNT(DISTINCT review_creation_date) > 1
