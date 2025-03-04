1c46d6c855fdfe378c9bdad92250b44c73139e81
MykolaKud <kudinov.myk@gmail.com>
2025-03-04 10:07:12 +0200

    Update 9. Average_freight_value_top10cities.sql

--- Analysis/9. Average_freight_value_top10cities.sql
+++ Analysis/9. Average_freight_value_top10cities.sql
@@ -6,5 +6,4 @@ JOIN olist_orders_dataset ood ON ooid.order_id = ood.order_id
 JOIN olist_customers_dataset ocd ON ood.customer_id = ocd.customer_id
 WHERE ooid.freight_value > 0
 GROUP BY ocd.customer_state
-ORDER BY average_freight_value DESC
-LIMIT 7;
+ORDER BY average_freight_value DESC;
